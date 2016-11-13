91e8f3a1f3a3bb6bd3282344acda024e24c5bc00
Abner Ferreira Santos <abnerfs@gmail.com>
2016-11-13 15:09:23 -0200

    Correção exemplo2

--- exemplo2.js
+++ exemplo2.js
@@ -1,18 +1,12 @@
-
-
-
-
-//No EcmaScript 5 para que variáveis dentro de uma função estejam disponíveis fora da função devemos utilizar uma
-//Immideately invoked function expression (IIFE) que é uma função que será chamada assim que for declarada como no exemplo abaixo 
+//Immideately invoked function expression (IIFE) é uma função que será chamada assim que for declarada como no exemplo abaixo 
 
 //IIFE
-
-(function counter2(){
-    for(var i = 0;i < 10;i++){
+(function (){
+    //Se não declararmos a variável i ela será declarada globalmente e estará disponível fora da função
+    for(i = 0;i < 10;i++){
         console.log(i);
     }
-})()
+})();
 
-
-//Assim a variável i estará disponível fora da função onde foi declarada
-console.log('Valor de i', i);
+//Variável i disponível globalmente
+console.log('Valor de i', i)
